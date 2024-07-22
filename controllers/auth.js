710fbab67e4a6b0057459674bb2ccfc5eb0e725d
safaminhaj <safaminhaj11@gmail.com>
2024-07-22 22:41:56 +0530

    update user controller completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -43,7 +43,34 @@ const login = async (req, res) => {
   });
 };
 
+const updateUser = async (req, res) => {
+  const { email, name, lastName, location } = req.body;
+  if (!email || !name || !lastName || !location) {
+    throw new BadRequestError("Please fill all fields");
+  }
+
+  const user = await User.findOne({ _id: req.user.userId });
+  user.email = email;
+  user.name = name;
+  user.lastName = lastName;
+  user.location = location;
+
+  await user.save();
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      name: user.name,
+      lastName: user.lastName,
+      location: user.location,
+      token,
+    },
+  });
+};
 module.exports = {
   register,
   login,
+  updateUser,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const router = express.Router()
-const { register, login } = require('../controllers/auth')
-router.post('/register', register)
-router.post('/login', login)
-
-module.exports = router
+const express = require("express");
+const router = express.Router();
+const { register, login, updateUser } = require("../controllers/auth");
+const authenticateUser = require("../middleware/authentication");
+router.post("/register", register);
+router.post("/login", login);
+router.patch("/updateUser", authenticateUser, updateUser);
+module.exports = router;
